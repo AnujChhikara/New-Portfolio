ae88f47e5dfdb66595136ed6e22b79428a9f53b3
AnujChhikara <anujchhikara777@gmail.com>
2024-06-21 13:30:54 +0530

    minor styling improvement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,43 +3,67 @@ import Projects from "@/components/Projects";
 import Tech from "@/components/Tech";
 import Image from "next/image";
 
-
 export default function Home() {
   return (
-   <main className="flex flex-col md:space-y-32 sm:space-y-12 h-screen  ">
-    {/* main section */}
-    <div className="bg-black pb-8 md:px-20 sm:px-4 md:pt-20 sm:pt-4">
-    <div className="flex flex-col justify-start space-y-4">
-      {/* image */}
-      <div className="">
-        <Image width={128} className="rounded-md bg-[#1b1b1b] p-1  w-32" height={128} src="https://res.cloudinary.com/dlahahicg/image/upload/v1712917455/zman4v8yaqtvixmnthmi.jpg" alt="avatar img" />
-      </div>
-      <h1 className="font-bold text-xl mt-2">Hello!</h1>
-      <div className="text-[#a3a3a3]">
-        <h2 className="font-bold underline text-white  mb-2 text-xl">I&apos;m Anuj </h2>
-        <p className="md:w-1/2">
-          I&apos;m a Full Stack Web Developer. I&apos;ve been honing my coding skills for a couple of years now, diving deep into various technologies to craft seamless digital experiences. My toolkit primarily includes <span className="underline">React</span>, <span className="underline">Next.js</span>, and <span className="underline">Node.js</span>, with occasional adventures in <span className="underline">Python</span>. Whether it&apos;s crafting captivating front-end interfaces or architecting robust back-end systems, I thrive on making things work smoothly.
-          . Whether it&apos;s bringing your vision to life or tackling complex technical challenges, I&apos;m here to make it happen. Let&apos;s collaborate and build some truly awesome stuff together! 🚀
-          </p>
-      </div>
-      
-    </div>
-     <ContactMe/>
-    </div>
-   {/* Tech/framworks */}
-   <div className="md:px-20 sm:px-4">
-  <h2 className="md:text-3xl sm:text-2xl font-semibold mb-8">{'<Languages & Frameworks>'}</h2>
-  
-  <Tech/>
-</div>
-  {/* Projects */}
-    <Projects/>
+    <main className='flex flex-col bg-gradient-to-b from-zinc-800 to-black  h-screen'>
+      {/* main section */}
+      <div className=' pb-8 md:px-20 flex flex-col space-y-4 items-center justify-center sm:px-2 text-center md:pt-20 sm:pt-12 '>
+        {/* image */}
+        <div className=' flex flex-col space-y-4 justify-center items-center'>
+          <div className='flex flex-col items-center justify-center'>
+            <Image
+              width={128}
+              className='rounded-full bg-[#5f5f5f] p-0.5  w-32'
+              height={128}
+              src='https://res.cloudinary.com/dlahahicg/image/upload/v1712917455/zman4v8yaqtvixmnthmi.jpg'
+              alt='avatar img'
+            />
+            <h2 className='font-semibold text-3xl  text-white tracking-widest'>
+              I&apos;m Anuj{" "}
+            </h2>
+          </div>
 
-  {/* footer */}
+          <div className='text-[#c7c7c7]'>
+            <p className='md:w-[800px] sm:text-sm md:text-base'>
+              I&apos;m a Full Stack Web Developer. I&apos;ve been honing my
+              coding skills for a couple of years now, diving deep into various
+              technologies to craft seamless digital experiences. My toolkit
+              primarily includes <span className='underline'>React</span>,{" "}
+              <span className='underline'>Next.js</span>, and{" "}
+              <span className='underline'>Node.js</span>, with occasional
+              adventures in <span className='underline'>Python</span>. Whether
+              it&apos;s crafting captivating front-end interfaces or
+              architecting robust back-end systems, I thrive on making things
+              work smoothly. . Whether it&apos;s bringing your vision to life or
+              tackling complex technical challenges, I&apos;m here to make it
+              happen. Let&apos;s collaborate and build some truly awesome stuff
+              together! 🚀
+            </p>
+          </div>
+        </div>
+        <ContactMe />
+      </div>
+      {/* Tech/framworks */}
+      <div className='md:px-20 sm:px-4 flex flex-col items-center justify-center py-20'>
+        <h2 className='md:text-6xl sm:text-4xl font-semibold mb-2'>
+          Skills and Tools
+        </h2>
+        <p className='text-[#a3a3a3] text-sm font-semibold text-center'>
+          Skills, Tools, and Technologies I Use to Bring Products to Life.
+        </p>
 
- <footer className=" w-full text-center py-4">
-    <p className="text-[#a3a3a3]">&copy; 2024 Anuj Chhhikara. All rights reserved.</p>
-</footer>
-   </main>
+        <Tech />
+      </div>
+      {/* Projects */}
+      <Projects />
+
+      {/* footer */}
+
+      <footer className=' w-full text-center pt-16 pb-4 '>
+        <p className='text-[#a3a3a3]'>
+          &copy; 2024 Anuj Chhhikara. All rights reserved.
+        </p>
+      </footer>
+    </main>
   );
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,45 +1,47 @@
-import React from 'react'
-import ProjectCard from './ProjectCard'
+import React from "react";
+import ProjectCard from "./ProjectCard";
 
 export default function Projects() {
   return (
-    <div className='md:px-20 sm:px-4 flex flex-col items-start space-y-4'>
-        <h3 className=' text-2xl  font-semibold'>{'<Projects>'}</h3>
-        <div className='md:grid md:grid-cols-2 sm:grid sm:grid-cols-1 gap-8'>
-
-        <ProjectCard ProjectName='Vidloom' 
-        Brief='The objective of this project is to develop a video sharing platform similar to YouTube, where users can upload their videos and watch videos uploaded by other users. '
-        GithubLink='https://github.com/AnujChhikara/frontend-yt'
-        LiveLink='https://vidloom.vercel.app/'
-        Tech={["React","Redux","Shadcn Ui","Express","MongoDB"]}
-        VideoUrl='https://res.cloudinary.com/dlahahicg/video/upload/v1714749400/video_20240418_212221_edit_d57rms.mp4'
+    <div className='md:px-20 sm:px-4  flex flex-col items-center py-16 space-y-4'>
+      <h3 className=' text-6xl pb-4  font-semibold'>Projects</h3>
+      <div className='md:grid md:grid-cols-2 sm:grid sm:grid-cols-1 gap-8'>
+        <ProjectCard
+          ProjectName='Vidloom'
+          Brief='The objective of this project is to develop a video sharing platform similar to YouTube, where users can upload their videos and watch videos uploaded by other users. '
+          GithubLink='https://github.com/AnujChhikara/frontend-yt'
+          LiveLink='https://vidloom.vercel.app/'
+          Tech={["React", "Redux", "Shadcn Ui", "Express", "MongoDB"]}
+          VideoUrl='https://res.cloudinary.com/dlahahicg/video/upload/v1714749400/video_20240418_212221_edit_d57rms.mp4'
         />
 
-         <ProjectCard ProjectName='Speedexx' 
-        Brief='The objective of this project is to develop an engaging and user-friendly e-commerce website. '
-        GithubLink='https://github.com/AnujChhikara/speedexClone'
-        LiveLink='https://speedex-clone.vercel.app/'
-        Tech={["React","Redux","Tailwind CSS"]}
-        VideoUrl='notalf '
+        <ProjectCard
+          ProjectName='Speedexx'
+          Brief='The objective of this project is to develop an engaging and user-friendly e-commerce website. '
+          GithubLink='https://github.com/AnujChhikara/speedexClone'
+          LiveLink='https://speedex-clone.vercel.app/'
+          Tech={["React", "Redux", "Tailwind CSS"]}
+          VideoUrl='notalf '
         />
 
-         <ProjectCard ProjectName='Chess' 
-        Brief='The aim of this project is to create an immersive and user-centric online chess platform.'
-        GithubLink='https://github.com/AnujChhikara/Chess'
-        LiveLink='/'
-        Tech={["React","Redux","Socket.io","Express","MongoDB"]}
-        VideoUrl='notalf '
+        <ProjectCard
+          ProjectName='Chess'
+          Brief='The aim of this project is to create an immersive and user-centric online chess platform.'
+          GithubLink='https://github.com/AnujChhikara/Chess'
+          LiveLink='/'
+          Tech={["React", "Redux", "Socket.io", "Express", "MongoDB"]}
+          VideoUrl='notalf '
         />
 
-         <ProjectCard ProjectName='DevVault' 
-        Brief=' Your go-to platform for accessing and sharing reusable code snippets effortlessly. Join now and streamline your development process!'
-        GithubLink='https://github.com/AnujChhikara/Vault'
-        LiveLink='https://www.devvault.xyz/'
-        Tech={["NextJS","Redux","Aceternity UI","MongoDB"]}
-        VideoUrl='notalf '
+        <ProjectCard
+          ProjectName='DevVault'
+          Brief=' Your go-to platform for accessing and sharing reusable code snippets effortlessly. Join now and streamline your development process!'
+          GithubLink='https://github.com/AnujChhikara/Vault'
+          LiveLink='https://www.devvault.xyz/'
+          Tech={["NextJS", "Redux", "Aceternity UI", "MongoDB"]}
+          VideoUrl='notalf '
         />
-       
-        </div>
+      </div>
     </div>
-  )
+  );
 }
