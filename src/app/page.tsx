a2c203219a1bfb4adbdc9ac3a558cae5125db697
AnujChhikara <anujchhikara777@gmail.com>
2025-02-22 14:42:42 +0530

    feat/added blob gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,37 @@
-"use client";
 import ContactMe from "@/components/ContactMe";
 import Projects from "@/components/Projects";
 import Tech from "@/components/Tech";
-import { Spotlight } from "@/components/ui/spotlight";
-import { Github, Linkedin, Moon, Sun, Twitter } from "lucide-react";
+import { Github, Linkedin, Twitter } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
 
 export default function Home() {
-  const [isDark, setIsDark] = useState(true);
-
-  const toggleTheme = () => {
-    setIsDark(!isDark);
-  };
   return (
     <main
-      className={`flex flex-col h-screen transition-colors duration-500 ease-in-out ${
-        isDark ? "bg-[#000000] text-white" : "bg-[#c4c4c4] text-zinc-900"
+      className={`flex flex-col text-white h-screen transition-colors duration-500 ease-in-out
       }`}
     >
-      <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill={isDark ? "white" : "none"}
-      />
-      <div className="flex items-center justify-end p-4 pb-0">
-        <button
-          onClick={toggleTheme}
-          className={`relative w-12 h-7 rounded-full p-1 transition-colors duration-300 ease-in-out ${
-            isDark ? "bg-zinc-800 text-white" : "bg-zinc-400 text-black"
-          }`}
-          aria-label="Toggle theme"
-        >
-          <span
-            className={`absolute inset-0.5 w-6 h-6 rounded-full flex items-center justify-center transition-transform duration-300 ease-in-out ${
-              isDark ? "bg-zinc-900 translate-x-5" : "bg-yellow-400"
-            }`}
-          >
-            {isDark ? (
-              <Moon className="w-3 h-3 text-white" />
-            ) : (
-              <Sun className="w-3 h-3 text-white" />
-            )}
-          </span>
-        </button>
+      <div className='blob-cont'>
+        <div className='yellow blob'></div>
+        <div className='red blob'></div>
+        <div className='green blob'></div>
       </div>
       {/* main section */}
-      <section className="pb-8 md:px-20 flex flex-col space-y-4 items-center justify-center sm:px-2 text-center md:pt-20 sm:pt-8 ">
+      <section className='pb-8 md:px-20 flex flex-col space-y-4 items-center justify-center sm:px-2 text-center md:pt-20 sm:pt-8 '>
         {/* image */}
-        <div className=" flex sm:flex-col md:flex-row-reverse sm:space-y-6 md:space-y-0   sm:pb-4 sm:pt-4  md:pb-12 md:pt-12">
+        <div className=' flex sm:flex-col md:flex-row-reverse sm:space-y-6 md:space-y-0   sm:pb-4 sm:pt-4  md:pb-12 md:pt-12'>
           <Image
             width={150}
-            className=" bg-[#5f5f5f] p-0.5 hover:bg-zinc-400 duration-1000 md:mx-12 rounded-2xl shadow-md shadow-white w-24  md:w-36"
+            className=' bg-[#5f5f5f] p-0.5 hover:bg-zinc-400 duration-1000 md:mx-12 rounded-2xl shadow-md shadow-white w-24  md:w-36'
             height={150}
-            src="https://res.cloudinary.com/dlahahicg/image/upload/v1712917455/zman4v8yaqtvixmnthmi.jpg"
-            alt="avatar img"
+            src='https://res.cloudinary.com/dlahahicg/image/upload/v1712917455/zman4v8yaqtvixmnthmi.jpg'
+            alt='avatar img'
           />
-          <div className="md:w-96 sm:w-80 flex flex-col  justify-center space-y-2">
-            <h1 className=" text-left  rounded-lg items-center font-bold ">
+          <div className='md:w-96 sm:w-80 flex flex-col  justify-center space-y-2'>
+            <h1 className=' text-left  rounded-lg items-center font-bold '>
               Anuj | Full Stack Developer
             </h1>
-            <hr className=" w-64 border-gray-500" />
-            <h2
-              className={`text-md ${
-                isDark ? "text-gray-300" : "text-zinc-800"
-              } text-left`}
-            >
+            <hr className=' w-64 border-gray-500' />
+            <h2 className={`text-md text-gray-300 text-left`}>
               I love building websites and programming. My goal is to create
               efficient, user-centric web solutions that excel in both design
               and performance.
@@ -76,18 +43,14 @@ export default function Home() {
       </section>
 
       {/* Projects */}
-      <Projects isDark={isDark} />
+      <Projects />
 
       {/* Tech/frameworks */}
-      <section className="md:px-20 bg-inherit text-inherit sm:px-4 flex flex-col items-center justify-center py-20">
-        <h2 className="md:text-6xl sm:text-4xl font-semibold mb-2">
+      <section className='md:px-20 bg-inherit text-inherit sm:px-4 flex flex-col items-center justify-center py-20'>
+        <h2 className='md:text-6xl sm:text-4xl font-semibold mb-2'>
           Skills and Tools
         </h2>
-        <p
-          className={`${
-            isDark ? "text-[#a3a3a3]" : "text-zinc-600"
-          }text-sm font-semibold text-center`}
-        >
+        <p className={`text-[#a3a3a3] text-sm font-semibold text-center`}>
           Skills, Tools, and Technologies I Use to Bring Products to Life.
         </p>
 
@@ -96,66 +59,47 @@ export default function Home() {
 
       {/* footer */}
 
-      <footer className="flex flex-col bg-inherit text-inherit space-y-2 pt-8 sm:px-4 md:px-0 justify-center items-center">
-        <p
-          className={`text-3xl  font-bold ${
-            isDark ? "text-zinc-300" : "text-zinc-800"
-          }`}
-        >
+      <footer className='flex flex-col bg-inherit text-inherit space-y-2 pt-8 sm:px-4 md:px-0 justify-center items-center'>
+        <p className={`text-3xl  font-bold text-zinc-300`}>
           Let&lsquo;s Collaborate
         </p>
-        <p
-          className={`pb-10 text-center ${
-            isDark ? "text-zinc-400" : "text-zinc-700"
-          }`}
-        >
+        <p className={`pb-10 text-center text-zinc-400`}>
           Hey there! Want to collaborate with me? Let&lsquo;s join forces and
           make magic happen! 💫
         </p>
         <a
-          href="mailTo:anujchhikara07@gmail.com"
-          className=" p-2 rounded-lg shadow-inner text-white hover:opacity-80  duration-500 shadow-white font-semibold bg-gradient-to-r from-stone-500 via-stone-600 to-stone-900 "
+          href='mailTo:anujchhikara07@gmail.com'
+          className=' p-2 rounded-lg shadow-inner text-white hover:opacity-80  duration-500 shadow-white font-semibold bg-gradient-to-r from-stone-500 via-stone-600 to-stone-900 '
         >
           Contact Me
         </a>
-        <div className="mt-10 w-auto flex flex-row justify-center">
+        <div className='mt-10 w-auto flex flex-row justify-center'>
           <div>
-            <hr className={`my-3 ${isDark ? "" : "border border-zinc-600"}`} />
-            <div className="flex h-5 items-center md:space-x-4   text-sm">
+            <hr className={`my-3 `} />
+            <div className='flex h-5 items-center md:space-x-4   text-sm'>
               <a
-                className={`flex items-center space-x-1 ${
-                  isDark ? "hover:text-gray-300" : "hover:text-gray-700"
-                } duration-500 hover:underline-offset-2 hover:underline`}
-                href="https://twitter.com/AnujChhikara07"
+                className={`flex items-center space-x-1  hover:text-gray-300
+                duration-500 hover:underline-offset-2 hover:underline`}
+                href='https://twitter.com/AnujChhikara07'
               >
                 <Twitter />
                 <p>Twitter</p>
               </a>
               <hr
-                className={`rotate-90 w-5 ${
-                  isDark ? "bg-white" : "bg-zinc-900 border-zinc-600"
-                }`}
+                className={`rotate-90 w-5 bg-white
+                `}
               />
               <a
-                className={`flex items-center space-x-1 ${
-                  isDark ? "hover:text-gray-300" : "hover:text-gray-700"
-                } duration-500 hover:underline-offset-2 hover:underline`}
-                href="https://github.com/AnujChhikara"
+                className={`flex items-center space-x-1  hover:text-gray-300  duration-500 hover:underline-offset-2 hover:underline`}
+                href='https://github.com/AnujChhikara'
               >
-                {" "}
                 <Github />
-                <p>Github</p>{" "}
+                <p>Github</p>
               </a>
-              <hr
-                className={`rotate-90 w-5 ${
-                  isDark ? "bg-white" : "bg-zinc-900 border-zinc-600"
-                }`}
-              />
+              <hr className={`rotate-90 w-5 bg-white `} />
               <a
-                className={`flex items-end space-x-1 ${
-                  isDark ? "hover:text-gray-300" : "hover:text-gray-700"
-                } duration-500 hover:underline-offset-2 hover:underline`}
-                href="https://in.linkedin.com/in/anuj-chhikara-webdeveloper"
+                className={`flex items-end space-x-1 hover:text-gray-300  duration-500 hover:underline-offset-2 hover:underline`}
+                href='https://in.linkedin.com/in/anuj-chhikara-webdeveloper'
               >
                 <Linkedin />
                 <p>LinkedIn</p>
@@ -165,9 +109,8 @@ export default function Home() {
         </div>
 
         <div
-          className={`text-sm  pt-4 pb-4 ${
-            isDark ? "text-zinc-400" : "text-zinc-700"
-          }`}
+          className={`text-sm  pt-4 pb-4
+           text-zinc-400`}
         >
           All Rights Reserved &copy; 2024
         </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,50 +1,46 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 
-export default function Projects({ isDark }: { isDark: boolean }) {
+export default function Projects() {
   return (
     <div
       className={`md:px-20 sm:px-4  flex flex-col items-center md:py-16 sm:pb-8  space-y-4 text-inherit bg-inherit`}
     >
-      <h3 className="md:text-6xl sm:text-4xl font-semibold mb-6">Projects</h3>
-      <div className=" md:grid-cols-2 sm:grid sm:grid-cols-1 gap-8">
+      <h3 className='md:text-6xl sm:text-4xl font-semibold mb-6'>Projects</h3>
+      <div className=' md:grid-cols-2 sm:grid sm:grid-cols-1 gap-8'>
         <ProjectCard
-          isDark={isDark}
-          ProjectName="Vidloom"
-          Brief="The objective of this project is to develop a video sharing platform similar to YouTube, where users can upload their videos and watch videos uploaded by other users. "
-          GithubLink="https://github.com/AnujChhikara/frontend-yt"
-          LiveLink="https://vidloom.vercel.app/"
+          ProjectName='Vidloom'
+          Brief='The objective of this project is to develop a video sharing platform similar to YouTube, where users can upload their videos and watch videos uploaded by other users. '
+          GithubLink='https://github.com/AnujChhikara/frontend-yt'
+          LiveLink='https://vidloom.vercel.app/'
           Tech={["React", "Redux", "Shadcn Ui", "Express", "MongoDB"]}
-          VideoUrl="https://res.cloudinary.com/dlahahicg/video/upload/v1714749400/video_20240418_212221_edit_d57rms.mp4"
+          VideoUrl='https://res.cloudinary.com/dlahahicg/video/upload/v1714749400/video_20240418_212221_edit_d57rms.mp4'
         />
 
         <ProjectCard
-          isDark={isDark}
-          ProjectName="DevVault"
-          Brief=" Your go-to platform for accessing and sharing reusable code snippets effortlessly. Join now and streamline your development process!"
-          GithubLink="https://github.com/AnujChhikara/Vault"
-          LiveLink="https://www.devvault.xyz/"
+          ProjectName='DevVault'
+          Brief=' Your go-to platform for accessing and sharing reusable code snippets effortlessly. Join now and streamline your development process!'
+          GithubLink='https://github.com/AnujChhikara/Vault'
+          LiveLink='https://www.devvault.xyz/'
           Tech={["NextJS", "Redux", "Aceternity UI", "MongoDB"]}
-          VideoUrl="https://res.cloudinary.com/dlahahicg/video/upload/v1719840437/Untitled__Jun_24_2024_08_12_AM_dyefph.webm"
+          VideoUrl='https://res.cloudinary.com/dlahahicg/video/upload/v1719840437/Untitled__Jun_24_2024_08_12_AM_dyefph.webm'
         />
         <ProjectCard
-          isDark={isDark}
-          ProjectName="Speedexx"
-          Brief="The objective of this project is to develop an engaging and user-friendly e-commerce website. "
-          GithubLink="https://github.com/AnujChhikara/speedexClone"
-          LiveLink="https://speedex-clone.vercel.app/"
+          ProjectName='Speedexx'
+          Brief='The objective of this project is to develop an engaging and user-friendly e-commerce website. '
+          GithubLink='https://github.com/AnujChhikara/speedexClone'
+          LiveLink='https://speedex-clone.vercel.app/'
           Tech={["React", "Redux", "Tailwind CSS"]}
-          VideoUrl="https://res.cloudinary.com/dlahahicg/video/upload/v1719840308/speedex_ayeqh6.mp4"
+          VideoUrl='https://res.cloudinary.com/dlahahicg/video/upload/v1719840308/speedex_ayeqh6.mp4'
         />
 
         <ProjectCard
-          isDark={isDark}
-          ProjectName="Reqium"
-          Brief="A Chrome extension for efficient REST API testing, featuring Bulk Mode to send multiple requests simultaneously for streamlined testing."
-          GithubLink="https://github.com/AnujChhikara/ReqiumAPI"
-          LiveLink="https://reqium.vercel.app/"
+          ProjectName='Reqium'
+          Brief='A Chrome extension for efficient REST API testing, featuring Bulk Mode to send multiple requests simultaneously for streamlined testing.'
+          GithubLink='https://github.com/AnujChhikara/ReqiumAPI'
+          LiveLink='https://reqium.vercel.app/'
           Tech={["React", "Tailwind CSS"]}
-          VideoUrl="https://res.cloudinary.com/dlahahicg/video/upload/v1733330655/videoplayback_yzh7n1.mp4"
+          VideoUrl='https://res.cloudinary.com/dlahahicg/video/upload/v1733330655/videoplayback_yzh7n1.mp4'
         />
       </div>
     </div>
