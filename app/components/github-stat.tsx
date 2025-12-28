2ca5cd44cf4c3d7c771588d32e9b9c0921fc0bcd
anuj chhikara <anujchhikara777@gmail.com>
2025-12-29 03:49:06 +0530

    feat: integrate GitHub statistics display using react-github-calendar, update homepage layout, and remove unused welcome component and logos

---
+++ app/components/github-stat.tsx
@@ -0,0 +1,30 @@
+import { GitHubCalendar } from "react-github-calendar";
+
+export function GithubStats() {
+  return (
+    <GitHubCalendar
+      username="anujchhikara"
+      showTotalCount={true}
+      fontSize={12}
+      tooltips={{
+        activity: {
+          text: (activity) =>
+            `${activity.level} activities on ${activity.date}`,
+          placement: "top",
+          offset: 6,
+          hoverRestMs: 300,
+        },
+        colorLegend: {
+          text: (level) => `Level ${level}`,
+          placement: "top",
+          offset: 6,
+          hoverRestMs: 300,
+        },
+      }}
+      theme={{
+        light: ["#f5f5f5", "#e5e5e5", "#d4d4d4", "#a3a3a3", "#737373"],
+        dark: ["#262626", "#404040", "#525252", "#737373", "#a3a3a3"],
+      }}
+    />
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,54 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { GithubStats } from "~/components/github-stat";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Portfolio" },
+    { name: "description", content: "My Portfolio" },
   ];
 }
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
-}
-
-export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+export default function Home() {
+  return (
+    <div
+      style={{
+        backgroundImage: "url(/noise-black.webp)",
+        width: "100vw",
+        height: "100vw",
+      }}
+    >
+      <div className="flex flex-col items-center justify-center max-w-3xl mx-auto space-y-12">
+        {/* header section */}
+        <div className="flex flex-col items-center justify-center">
+          <img
+            src="/header.webp"
+            alt="logo"
+            className="w-full grayscale"
+            style={{
+              maskImage:
+                "linear-gradient(to right, transparent 0%, black 10%, black 90%, transparent 100%), linear-gradient(to bottom, black 0%, black 85%, transparent 100%)",
+              WebkitMaskImage:
+                "linear-gradient(to right, transparent 0%, black 10%, black 90%, transparent 100%), linear-gradient(to bottom, black 0%, black 85%, transparent 100%)",
+              maskComposite: "intersect",
+              WebkitMaskComposite: "source-in",
+            }}
+          />
+          <div className="flex flex-row items-end justify-start space-x-4 -mt-2 w-full">
+            <img
+              src="/profile.jpg"
+              alt="logo"
+              className="w-24 h-24 rounded-md border border-neutral-300 p-0.5 dark:border-neutral-700 shadow"
+            />
+            <div className="flex flex-col items-start justify-start">
+              <h1 className="text-2xl font-bold">Anuj Chhikara</h1>
+              <p className="text-sm text-gray-500 dark:text-gray-400">
+                Software Engineer
+              </p>
+            </div>
+          </div>
+        </div>
+        <GithubStats />
+      </div>
+    </div>
+  );
 }
